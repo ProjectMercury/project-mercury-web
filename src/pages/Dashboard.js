7b380507e7f94d4a51928cbf363bc1cd70e1df57
Francis Bulus <notchera@gmail.com>
2019-10-16 14:17:36 +0100

    built out user profile and button components with Ant

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Button, Avatar } from "antd";
+import { userData } from "../data/mockData";
+import "antd/dist/antd.css";
 
 const Dashboard = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="left-side">
+        <div className="user-profile">
+          <Avatar size={150} icon="user" />
+          <h1>{userData.name}</h1>
+          <p>Lambda School</p>
         </div>
-    );
+        <Button type="primary" icon="plus" size="large">
+          Create Form
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
